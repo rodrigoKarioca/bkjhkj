093f2d8e2c6cb97a8b1f906490adc46d6cf62e0b
RODRIGO RIBEIRO CARIOCA <32111439800@jpmorgan.sp>
2017-10-18 20:54:40 -0200

    includes e variaveis incluidas

--- bkjhkj.ino
+++ bkjhkj.ino
@@ -1,4 +1,18 @@
-// Testando o git
+// Testando o git com includes e defines
+
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <Servo.h> 
+
+#define SERVO 3 // O servo está na porta digital 3
+#define GARAGEBUTTON 5 // O botão de comando do portão está na porta digital 5
+#define LIGHTBUTTON 4 // O botão de comando da led está na porta digital 4
+#define LIGHT 6 // O led que simula a lampada está na porta 6
+#define OFF LOW
+#define ON HIGH
+#define OPEN HIGH
+#define CLOSED LOW
 
 void setup() {
   // put your setup code here, to run once:
